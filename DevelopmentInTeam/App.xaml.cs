ebb6e0825b3718d3a7bfaf2e6c4aa7400bbfe1c6
Michael_Lopez <lopezpms@gmail.com>
2023-02-01 13:10:01 -0500

    Add project files.

---
+++ DevelopmentInTeam/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DevelopmentInTeam;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DevelopmentInTeam/Models/Lichess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentInTeam.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public Product {string
+        {
+        ProductId= productId;
+        ProductName=ProductName;
+        }
+    }
+}
