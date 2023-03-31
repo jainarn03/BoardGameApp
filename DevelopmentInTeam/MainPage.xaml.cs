1a4d5a6b080c911331e0dd54175cebddd765b94b
aleksbrsc <aleksbrsc@gmail.com>
2023-03-30 22:06:06 -0400

    test commit

--- DevelopmentInTeam/MainPage.xaml.cs
+++ DevelopmentInTeam/MainPage.xaml.cs
@@ -2,11 +2,28 @@
 
 public partial class MainPage : ContentPage
 {
+    CarouselCover _carouselCover;
+    public CarouselCover Carousel => _carouselCover;
 	public MainPage()
 	{
 		InitializeComponent();
+        _carouselCover = new CarouselCover();
 
-
+        BindingContext = this;
 	}
+
+    public class CarouselCover
+    {
+        /// <summary>
+        /// specified image URLs in string array assigned to ImageURLs property
+        /// </summary>
+        public IEnumerable<string> ImageURLs { get; } = new List<string>
+        {
+        "checkers_art.png",
+        "connectfour_art.png",
+        "tictactoe_art.png",
+        "tictactoe_art.png"
+        };
+    }
 }
 
