81da99063e583023899e773ed5ac7ceb599d461c
Mikeball1 <lopezpms@gmail.com>
2023-03-31 22:48:06 -0400

    Added event handlers for buttons

--- DevelopmentInTeam/Pages/TicTacToePage.xaml.cs
+++ DevelopmentInTeam/Pages/TicTacToePage.xaml.cs
@@ -6,4 +6,54 @@ public partial class TicTacToePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void TopLeft_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void TopMiddle_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void TopRight_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void MiddleLeft_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void MiddMiddle_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void MiddleRight_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BottomLeft_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BottomMiddle_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BottomRight_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void NewGame_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
