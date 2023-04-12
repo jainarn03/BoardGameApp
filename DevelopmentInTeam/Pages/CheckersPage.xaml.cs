b97339ef5642be787bf73b462f09d0ec3a731c5d
jainarn03 <jainarnav281@gmail.com>
2023-04-12 04:25:06 -0400

    Added clicked property for all image buttons. Made a 2D array in C# Added a region for all image buttons for better readability

--- DevelopmentInTeam/Pages/CheckersPage.xaml.cs
+++ DevelopmentInTeam/Pages/CheckersPage.xaml.cs
@@ -1,7 +1,9 @@
+// Arnav's Page
 namespace DevelopmentInTeam.Pages;
 
 public partial class CheckersPage : ContentPage
 {
+    int[,] _gameBoard = new int[8, 8]; 
 	public CheckersPage()
 	{
 		InitializeComponent();
@@ -16,4 +18,152 @@ public partial class CheckersPage : ContentPage
     {
         await Navigation.PushAsync(new MainPage());
     }
+
+    public void InitializeBoard()
+    {
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                _gameBoard[row, col] = 0;
+            }
+        }
+    }
+
+    #region Image Buttons
+
+    // red peices image buttons
+    private void square_B8(object sender, EventArgs e)
+    {
+    }
+    private void square_D8(object sender, EventArgs e)
+    {
+
+    }
+    private void square_F8(object sender, EventArgs e)
+    {
+
+    }
+    private void square_H8(object sender, EventArgs e)
+    {
+
+    }
+    private void square_A7(object sender, EventArgs e)
+    {
+
+    }
+    private void square_C7(object sender, EventArgs e)
+    {
+
+    }
+    private void square_E7(object sender, EventArgs e)
+    {
+
+    }
+    private void square_G7(object sender, EventArgs e)
+    {
+
+    }
+    private void square_B6(object sender, EventArgs e)
+    {
+
+    }
+    private void square_D6(object sender, EventArgs e)
+    {
+
+    }
+    private void square_F6(object sender, EventArgs e)
+    {
+
+    }
+    private void square_H6(object sender, EventArgs e)
+    {
+
+    }
+
+    //black peices image buttons
+    private void square_A3(object sender, EventArgs e)
+    {
+
+    }
+    private void square_C3(object sender, EventArgs e)
+    {
+
+    }
+    private void square_E3(object sender, EventArgs e)
+    {
+
+    }
+    private void square_G3(object sender, EventArgs e)
+    {
+
+    }
+    private void square_B2(object sender, EventArgs e)
+    {
+
+    }
+    private void square_D2(object sender, EventArgs e)
+    {
+
+    }
+    private void square_F2(object sender, EventArgs e)
+    {
+
+    }
+    private void square_H2(object sender, EventArgs e)
+    {
+
+    }
+    private void square_A1(object sender, EventArgs e)
+    {
+
+    }
+    private void square_C1(object sender, EventArgs e)
+    {
+
+    }
+    private void square_E1(object sender, EventArgs e)
+    {
+
+    }
+    private void square_G1(object sender, EventArgs e)
+    {
+
+    }
+
+    //non image buttons
+    private void square_A5(object sender, EventArgs e)
+    {
+
+    }
+    private void square_C5(object sender, EventArgs e)
+    {
+
+    }
+    private void square_E5(object sender, EventArgs e)
+    {
+
+    }
+    private void square_G5(object sender, EventArgs e)
+    {
+
+    }
+    private void square_B4(object sender, EventArgs e)
+    {
+
+    }
+    private void square_D4(object sender, EventArgs e)
+    {
+
+    }
+    private void square_F4(object sender, EventArgs e)
+    {
+
+    }
+    private void square_H4(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
 }
