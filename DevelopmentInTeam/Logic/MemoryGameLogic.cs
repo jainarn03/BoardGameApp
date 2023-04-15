de35ccf6acc74d2e09d85aa4324d8f91be9f95f5
aleksbrsc <aleksbrsc@gmail.com>
2023-04-14 22:28:46 -0400

    probably done memory game class, added FlipCard and Shuffle methods, added eventhandlers for all the buttons clicked, added my name to every page i made, also using the logic now in the codebehind

--- DevelopmentInTeam/Logic/MemoryGameLogic.cs
+++ DevelopmentInTeam/Logic/MemoryGameLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// ALEKS' PAGE
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -55,7 +57,7 @@ namespace DevelopmentInTeam.Logic
         private ObservableCollection<Card> _cards; // observable collection of all the cards in the game
         private Card _flippedCard;
         private int _matchedPairsCount; // number of matched card pairs
-        private bool _gameOver; // indicates whether the game is over //CHANGE
+        private bool _gameOver; // indicates whether the game is over 
 
         // corresponding getters/setters
         public ObservableCollection<Card> Cards
@@ -86,11 +88,78 @@ namespace DevelopmentInTeam.Logic
         /// class constructor
         /// </summary>
         public MemoryGame() 
-        { 
+        {
+            // Initializing _cards observable collection field with 6 card objects
+            _cards = new ObservableCollection<Card>()
+            {
+                new Card(1, "card_1.png", false),
+                new Card(2, "card_2.png", false),
+                new Card(3, "card_3.png", false),
+                new Card(4, "card_4.png", false),
+                new Card(5, "card_5.png", false),
+                new Card(6, "card_6.png", false)
+            };
+        }
 
+        /// <summary>
+        /// Method handling flipping cards: checks for a match with previously flipped card.
+        /// If a match exists, the cards remain flipped. If no match, the cards are flipped back
+        /// after delay of 1-2 seconds (may change). Game will end when all cards are paired
+        /// </summary>
+        /// <param name="card"></param>
+        public void FlipCard(Card card)
+        {
+            // checks whether card can be flipped or not if: game is not over, card flipped is not same as previously flipped card, 
+            if (!_gameOver && _flippedCard != card && !card.IsMatched)
+            {
+                // if no card previously flipped
+                if (_flippedCard == null)
+                {
+                    _flippedCard = card; 
+                    _flippedCard.IsMatched = true; // changes the card state to flipped
+                }
+                // second card flipped
+                else
+                {
+                    card.IsMatched = true; // change card state to flipped
+                    if (_flippedCard.ID == card.ID) // if matching IDs
+                    {
+                        _flippedCard.IsMatched = true;
+                        _matchedPairsCount++; // increments matched pairs by 1
+
+                        if (_matchedPairsCount == _cards.Count / 2) // if all cards paired, game is complete
+                            _gameOver = true;
+                    }
+                    // no matches
+                    else
+                    {
+                        // milliseconds delay method found:
+                        //https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.continuewith?view=net-8.0
+                        Task.Delay(2000).ContinueWith(t =>
+                        {
+                            _flippedCard.IsMatched = false; // flip both cards back
+                            card.IsMatched = false; // set matched to false
+                        });
+                    }
+
+                    _flippedCard = null; // reset flipped card
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shuffling method which uses Random and LINQ's OrderBy method to shuffle the observable collection
+        /// by ordering the current _cards by a randomly generated integer for each car then assigning
+        /// the new collection to _cards
+        /// first link takes straight to example found:
+        /// https://www.tutorialsteacher.com/linq/linq-sorting-operators-orderby-orderbydescending#:~:text=var%20studentsInAscOrder%20%3D%20studentList.OrderBy(s%20%3D%3E%20s.StudentName)%3B
+        /// https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.orderby?view=net-8.0
+        /// </summary>
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            _cards = new ObservableCollection<Card>(_cards.OrderBy(card => rng.Next()));
         }
     }
-
-
-
 }
--- DevelopmentInTeam/Pages/MemoryPage.xaml.cs
+++ DevelopmentInTeam/Pages/MemoryPage.xaml.cs
@@ -1,16 +1,93 @@
+// ALEKS' PAGE
+using DevelopmentInTeam.Logic;
+
 namespace DevelopmentInTeam.Pages;
 
 public partial class MemoryPage : ContentPage
 {
+    // declaring field for MemoryGame class and its corresponding property
+    MemoryGame _memoryGame;
+
+    public MemoryGame MemoryGame => _memoryGame;
+
 	public MemoryPage()
 	{
 		InitializeComponent();
+
+        // creating new instance of MemoryGame class
+        _memoryGame = new MemoryGame();
 	}
 
+    /// <summary>
+    /// 1-12 Card clicked EventHandlers region
+    /// </summary>
+    #region
+    private void Card11Clicked(object sender, EventArgs e)
+    {
+
+    }
+    private void Card12Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card13Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card21Clicked(object sender, EventArgs e)
+    {
+
+    }
+    private void Card22Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card23Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card31Clicked(object sender, EventArgs e)
+    {
+
+    }
+    private void Card32Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card33Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card41Clicked(object sender, EventArgs e)
+    {
+
+    }
+    private void Card42Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Card43Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    #endregion
+
+    /// <summary>
+    ///  new game clicked eventhandler reloads the page
+    /// </summary>
     private async void OnNewGameClicked(object sender, EventArgs e)
     {
         // adds page to the navigation stack and removes the old one
         await Navigation.PushAsync(new MemoryPage());
         Navigation.RemovePage(this);
     }
+
 }
